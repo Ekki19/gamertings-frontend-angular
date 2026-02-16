cec7dd879b7ce4609c8fb30a1afa672fc5b2dd45
Jan Ekgardt <jan.ekgardt@gmail.com>
2026-02-16 19:44:27 +0100

    add new search component

---
+++ src/app/components/search/search.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { GameService} from '../../services/game/game-service';
+import {FormsModule} from '@angular/forms';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.html',
+  styleUrl: './search.css',
+  imports: [
+    FormsModule,
+    Button
+  ]
+})
+export class Search {
+  searchTerm: string = '';
+  games: any[] = [];
+
+  constructor(private gameService: GameService) {}
+
+  onSearch() {
+    this.gameService.search(this.searchTerm).subscribe(data => {
+      this.games = data;
+    })
+  }
+
+}
